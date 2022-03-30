76c1f44b94939c2ab56b356799c22383f50c6b4f
Lipin Vyacheslav <vlipin@ics.perm.ru>
2022-03-30 14:06:30 +0500

    v1.3.2
    - fix options button in top bar

--- ext.js
+++ ext.js
@@ -375,18 +375,7 @@ function consoleGroupEnd() {
       }
 
       static settingsPageButtonClick() {
-        function onOpened() {
-          console.log(`Options page opened`);
-        }
-
-        function onError(error) {
-          console.log(`Error: ${error}`);
-        }
-
-        let opening = browser.runtime.openOptionsPage();
-        opening.then(onOpened, onError);
-
-        // browser.runtime.openOptionsPage().then(console.log).catch(console.error);
+        browser.runtime.sendMessage({ action: "openOptionsPage" });
       }
 
       static collapseClick(e) {
