7b7e1da32393e12910f177eacdfedc06b3548d75
Lipin Vyacheslav <vlipin@ics.perm.ru>
2022-03-31 19:11:10 +0500

    v1.3.4
    - fix saving options
    
    - make options shoning only on page

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 browser.runtime.onMessage.addListener(function (message) {
   switch (message.action) {
     case "settingsUpdated":
-      browser.tabs.query({ currentWindow: true, active: false }).then((tabs) => {
+      browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
         tabs.forEach((tab) => {
           browser.tabs.sendMessage(tab.id, { action: "settingsUpdated", data: message.data });
         });
